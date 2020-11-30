28cd796b0f22e81907e3f3e2609e9f1b6b8d2290
Martin <hectormartinluduena@gmail.com>
2020-11-30 09:47:48 -0300

    homepage and style items

---
+++ src/pages/homepage.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from "../component/directory/directory.component.jsx"
+import "./homepage.styles.scss";
+const HomPage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+export default HomPage
