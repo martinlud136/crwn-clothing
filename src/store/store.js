62343f091e7c98e7a7736d161fad84e946229baf
martin <hectormartinluduena@gmail.com>
2023-05-04 14:44:58 -0300

    se agrega redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger"
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -28,11 +14,15 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 //se ejecuta antes de que la accion del dispatch llege al reducer, me va a permitir ver la accion
 //y el estado antes y despues del dispatch
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean //esto es hermoso, filtro los false en un array
+);
+
+const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 //composicion de potenciadores, de esta forma es un proceso de combinar varias funciones "enhancers" en una sola función compuesta
 //que mejora el comportamiento de los "reducers" en Redux.
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 
