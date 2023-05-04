c0db528f6ed9917e0dd088bab7461ef091052840
martin <hectormartinluduena@gmail.com>
2023-05-04 15:48:28 -0300

    se agrega redux thunk a categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,18 +6,13 @@ import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoryArray));
-    };
-    getCategoriesMap();
+      dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
@@ -14,11 +15,18 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 //se ejecuta antes de que la accion del dispatch llege al reducer, me va a permitir ver la accion
 //y el estado antes y despues del dispatch
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(
   Boolean //esto es hermoso, filtro los false en un array
 );
 
-const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 //composicion de potenciadores, de esta forma es un proceso de combinar varias funciones "enhancers" en una sola función compuesta
 //que mejora el comportamiento de los "reducers" en Redux.
