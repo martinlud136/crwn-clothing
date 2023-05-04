0fe8c0183542f16a8f3606f8c8ec7fdb8f8d7ad8
martin <hectormartinluduena@gmail.com>
2023-05-04 11:55:36 -0300

    se agrega redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,28 +1,39 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import logger from "redux-logger"
 
 import { rootReducer } from "./root-reducer";
 
 const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-      return next(action);
-    }
-  
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-  
-    next(action);
-  
-    console.log('next state: ', store.getState());
-  };
-  
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 //se ejecuta antes de que la accion del dispatch llege al reducer, me va a permitir ver la accion
 //y el estado antes y despues del dispatch
-const middleWares = [loggerMiddleware]
+const middleWares = [loggerMiddleware];
 
-//composicion de potenciadores, de esta forma es un proceso de combinar varias funciones "enhancers" en una sola función compuesta 
+//composicion de potenciadores, de esta forma es un proceso de combinar varias funciones "enhancers" en una sola función compuesta
 //que mejora el comportamiento de los "reducers" en Redux.
-const composeEnhancers = compose(applyMiddleware(...middleWares))
+const composeEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composeEnhancers)
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
