d04238046eabd17f770971b2cc7f4053e4fbf11e
martin <hectormartinluduena@gmail.com>
2023-05-04 16:15:56 -0300

    se agrega spiner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,32 +1,40 @@
-import { Fragment, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Fragment, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import ProductCard from '../../components/product-card/product-card.component'
-import { selectCategoriesMap } from '../../store/categories/category.selector'
-import { CategoryContainer, CategoryTitle } from './category.styles'
+import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spiner.component";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/category.selector";
+import { CategoryContainer, CategoryTitle } from "./category.styles";
 
 const Category = () => {
-    const { category } = useParams()
-    const categoriesMap = useSelector(selectCategoriesMap)
-    const [products, setProducts] = useState(categoriesMap[category])
+  const { category } = useParams();
+  const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-    useEffect(()=>{
-        setProducts(categoriesMap[category])
-    },[category, categoriesMap])
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
-    return(
-        <Fragment>
-            <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
-                {products &&
-                products.map((product)=><ProductCard key={product.id} product={product}/>)   
-                }
-            </CategoryContainer>
+  return (
+    <Fragment>
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
+    </Fragment>
+  );
+};
 
-        </Fragment>
-    )
-
-}
-
-export default Category
+export default Category;
