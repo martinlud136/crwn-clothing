9c8e79d1d927d49232449c054cb6c66df055e040
martin <hectormartinluduena@gmail.com>
2023-07-03 09:04:15 -0300

    Updates

--- src/store/user/user.action.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
-import { USER_ACTIONS_TYPES } from "./user.types";  
-
-export const setCurrentUser = (user) =>
-createAction(USER_ACTIONS_TYPES.SET_CURRENT_USER, user);
